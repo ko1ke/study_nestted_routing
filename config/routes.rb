42e133be9ae59af5e38547921a99fc246261cf83
koji koike <k.koike@daitecjp.com>
2018-12-03 12:16:54 +0900

    nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :groups
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index'
+  resources :groups do
+    resources :users
+  end
 end
